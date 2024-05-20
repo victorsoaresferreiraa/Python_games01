024ab25ef0a6f2b54d88fed161be972a04f0aa11
Victor Soares Ferreira <victorsoaresferreira09@gmail.com>
2024-05-20 18:14:11 -0300

    arrumando algumas coisas no codigo

--- code/main.py
+++ code/main.py
@@ -2,21 +2,25 @@ import pygame
 
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-Running = pygame.window("lalala")
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Space shooter')
+running = True
+
+surf = pygame.Surface((100, 200))
 
 
-
-while Running:
+while running:
     #event loop
     for event in pygame.event.get():
-        if event.tipe == pygame.QUIT:
-            pygame.quit()
+        if event.type == pygame.QUIT:
+            running = False
+            
             
 
     # Draw the game
     #fill the window 
-    
+    display_surface.fill('darkgray')
+    display_surface.blit(surf, (100, 400))
     pygame.display.update()
     
         
