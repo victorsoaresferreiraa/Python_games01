e8039cfd635496a6511838e9873065d2392da652
Victor Soares Ferreira <victorsoaresferreira09@gmail.com>
2024-05-27 13:32:43 -0300

    atualização de alguns bugs

--- code/main.py
+++ code/main.py
@@ -1,40 +1,75 @@
 import pygame 
 from os.path import join
 
+#Importa o modulo random, que randomiza numeros
 from random import randint
 
+#Inicia o pygame
 pygame.init()
+#Variaveis que controlam o tamanho da tela
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+#Mostra o tamanho da tela
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+#Seta o nome que aparece quando inicia a tela
 pygame.display.set_caption('Space shooter')
 running = True
-clock = pygame.time.clock()
+#Variavel que conta tempo 
+clock = pygame.time.Clock()
 
-
+#Desenha a primeira interface do projeto, para que os outros sejam desenhados em cima
 surf = pygame.Surface((100, 200))
 surf.fill('orange')
 
+#Carrega o player, pegando dos arquivos 
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 player_direction = pygame.math.Vector2()
 player_speed = 300
 
 star_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
-star_position = [(randint (Q, WINDOW_WIDTH), radint(0, WINDOW_HEIGHT)) for i in range(20)]
+star_position = [(randint (0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
+meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
+meteor_react = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 
 while running:
+    dt = clock.tick() / 1000
+    
     #event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+            print(1)
+        if event.type == pygame.MOUSEMOTION:
+            player_rect.center = event.pos
+            
+        
+    #input
+    #print(pygame.mouse.get_rel())
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    player_direction = player_direction.normalize() if player_direction else player_direction
+    player_rect.center += player_direction * player_speed * dt
+    
             
             
 
     # Draw the game
     #fill the window 
     display_surface.fill('darkgray')
-    display_surface.blit(surf, (100, 400))
+    
+    for pos in star_position:
+        display_surface.blit(star_surf, pos)
+        
+    display_surface.blit(meteor_surf, meteor_react)
+    display_surface.blit(laser_surf, laser_rect)
+    display_surface.blit(player_surf, player_rect)
     pygame.display.update()
     
         
